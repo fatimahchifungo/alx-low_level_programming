723e3963fb02da2d17a9356dd971a17253bfbbc8
fatimahchifungo <fatimahng444@gmail.com>
2023-03-29 14:18:10 -0700

    0x06. C - More pointers, arrays and strings C

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,8 +1,7 @@
-#include #include "main.h"
-
+#include "main.h"
 /**
  * _strncat - Concatenates two strings using at most
- *            an inputted number of bytes from src.
+ * an inputted number of bytes from src.
  * @dest: The string to be appended upon.
  * @src: The string to be appended to dest.
  * @n: The number of bytes from src to be appended to dest.
@@ -11,11 +10,10 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int index = 0, dest_len = 0;
-
-	while (dest[index++])
-		dest_len++;
-	for (index = 0; src[index] && index < n; index++)
-		dest[dest_len++] = src[index];
-	return (dest);
+int index = 0, dest_len = 0;
+while (dest[index++])
+dest_len++;
+for (index = 0; src[index] && index < n; index++)
+dest[dest_len++] = src[index];
+return (dest);
 }
--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -20,5 +20,4 @@ p = &n;
 *(p + 5) = 98;
   /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
-return (0);
-}
+retur}
